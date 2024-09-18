e65ca1359ea3ff69ec248023b9452be71f753c55
BuschMath <47864618+BuschMath@users.noreply.github.com>
2024-09-18 09:35:23 -0500

    scientific

--- Ch3_Example_Home_FA24/Main.cpp
+++ Ch3_Example_Home_FA24/Main.cpp
@@ -3,11 +3,19 @@
 
 int main()
 {
+	float y = 21.052;
+
+	std::cout << "y = " << y << std::endl;
+	std::cout << std::showpoint << "y = " << y << std::endl;
+	std::cout << std::fixed << "y = " << y << std::endl;
+	std::cout << std::scientific << "y = " << y << std::endl;
+
 	float x = 1.01;
 
 	std::cout << "x = " << x << std::endl;
 	std::cout << std::showpoint << "x = " << x << std::endl;
 	std::cout << std::fixed << "x = " << x << std::endl;
+	std::cout << std::scientific << "x = " << x << std::endl;
 
 	return 0;
 }
